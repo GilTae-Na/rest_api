42ed12ddd67b1dfb136739ed85e61b121cd38197
nagiltae <nagt1997@gmail.com>
2023-11-22 20:06:02 +0900

    엑세스 토큰을 헤더로 전송하는 것 없앰, json 응답결과를 더 정밀하게 테스트

--- src/test/java/com/ll/rest_api/boundedContext/member/controller/MemberControllerTest.java
+++ src/test/java/com/ll/rest_api/boundedContext/member/controller/MemberControllerTest.java
@@ -19,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -48,14 +49,9 @@ class MemberControllerTest {
 
         // Then
         resultActions
-                .andExpect(status().is2xxSuccessful());
-
-        MvcResult mvcResult = resultActions.andReturn();
-
-        MockHttpServletResponse response = mvcResult.getResponse();
-
-        String authentication = response.getHeader("Authentication");
-
-        assertThat(authentication).isNotEmpty();
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.resultCode").value("S-1"))
+                .andExpect(jsonPath("$.msg").exists())
+                .andExpect(jsonPath("$.data.accessToken").exists());
     }
 }
